71ada273d893151554026534eaabce93edac8d05
Henrique Leite <hleite@uwo.ca>
2026-06-19 01:52:25 -0400

    Basic template, plus headshot

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Henrique", url=os.getenv("URL"), photo="henrique.jpg", switch_url="/kriti", switch_name="Kriti Behl")
+
+
+@app.route('/henrique')
+def henrique():
+    return render_template('index.html', title="Henrique", url=os.getenv("URL"), photo="henrique.jpg", switch_url="/kriti", switch_name="Kriti Behl")
+
+
+@app.route('/kriti')
+def kriti():
+    return render_template('index.html', title="Kriti Behl", url=os.getenv("URL"), photo="logo.jpg", switch_url="/henrique", switch_name="Henrique")
